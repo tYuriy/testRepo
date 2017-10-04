d46f0b82c2b891a3cfa17318dd8fc2d4df7acf34
aUser <t_yuriy@meta.ua>
2017-10-04 11:27:10 +0300

    First test commit

--- testClass.java
+++ testClass.java
@@ -2,5 +2,6 @@ public class testClass {
     public static void main(String[] args) {
         System.out.println("WTF");
         System.out.println("HI!!!");
+        System.out.println("updateTest");
     }
 }
