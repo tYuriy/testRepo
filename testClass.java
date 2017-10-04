2ecef8d2e840555a12da30b1ed801fff9cf1b198
aUser <t_yuriy@meta.ua>
2017-10-04 11:25:54 +0300

    First test commit

--- testClass.java
+++ testClass.java
@@ -1,5 +1,6 @@
 public class testClass {
     public static void main(String[] args) {
         System.out.println("WTF");
+        System.out.println("HI!!!");
     }
 }
--- testClass2.java
+++ testClass2.java
@@ -1,5 +1,5 @@
 public class testClass2 {
     public static void main(String[] args) {
-        System.out.println("WWW");
+        System.out.println("WWW.ukr.NET");
     }
 }
