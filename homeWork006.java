40cd120171ec497000bf3a8ece5a51ad2e955101
aUser <t_yuriy@meta.ua>
2017-10-05 13:41:48 +0300

    First test commit

---
+++ homeWork006.java
@@ -0,0 +1,31 @@
+public class homeWork006 {
+    public static void main(String[] args) {
+        /*Напишите программу, которая вычисляет сумму квадратов натуральных чисел:*/
+
+        int n = 20;
+        int rez=0;
+        int i;
+
+        for (i=1;i<=n;i++){
+            rez += i*i;
+        }
+        System.out.println(rez);
+
+        rez=0;
+        i=1;
+
+        do {
+            rez += i*i;
+            i++;
+        } while (i<=n);
+        System.out.println(rez);
+
+        rez=0;
+        i=1;
+
+        while (i<=n){
+            rez += i*i;
+            i++;}
+        System.out.println(rez);
+    }
+}
