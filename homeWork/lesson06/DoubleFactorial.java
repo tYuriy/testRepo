d32d7c57cb147a56362d4ab1337fb41354070363
aUser <t_yuriy@meta.ua>
2017-10-19 17:38:08 +0300

    First test commit

--- homeWork/lesson06/DoubleFactorial.java
+++ homeWork/lesson06/DoubleFactorial.java
@@ -3,8 +3,8 @@ package homeWork.lesson06;
 public class DoubleFactorial {
     //программа с методом для вычисления двойного факториала
     public static void main(String[] args) {
-        int n = 20;
-        System.out.println(calcFact(n));
+        int n = 5;
+        System.out.println("Двойной факториал числа " + n + " = " + calcFact(n));
     }
 
     static long calcFact (int a){
